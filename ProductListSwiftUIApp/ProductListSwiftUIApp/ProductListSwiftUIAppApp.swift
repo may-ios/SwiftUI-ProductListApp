b29a8672c2929afa6c778fe009ac43f7296417c6
kme <may@mycelebs.com>
2025-08-20 18:54:49 +0900

    feat: initial project setup

---
+++ ProductListSwiftUIApp/ProductListSwiftUIApp/ProductListSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListSwiftUIAppApp.swift
+//  ProductListSwiftUIApp
+//
+//  Created by kme on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductListSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProductListView()
+        }
+    }
+}
---
+++ ProductListSwiftUIApp/ProductListSwiftUIApp/Sources/Views/ProductListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductListView.swift
+//  ProductListSwiftUIApp
+//
+//  Created by kme on 8/20/25.
+//
+
+import SwiftUI
+
+// MARK: - ProductListView
+struct ProductListView: View {
+    var body: some View {
+        NavigationView {
+        }
+    }
+}
+
+#Preview {
+    ProductListView()
+}
