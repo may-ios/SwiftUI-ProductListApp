bc7c0e4e7e71b6814984eabd55ddd61fa4daf587
kme <akme762@naver.com>
2025-08-20 20:14:40 +0900

    feat: setup webview configuration and shared process pool

---
+++ ProductListSwiftUIApp/ProductListSwiftUIApp/Sources/WebComponents/WebConfiguration.swift
@@ -0,0 +1,27 @@
+//
+//  WebConfiguration.swift
+//  ProductListSwiftUIApp
+//
+//  Created by kme on 8/20/25.
+//
+
+import WebKit
+
+// MARK: - WebConfiguration
+// WKWebViewConfiguration을 래핑하는 설정 클래스
+final class WebConfiguration: ObservableObject {
+    let configuration: WKWebViewConfiguration
+    
+    init() {
+        configuration = WKWebViewConfiguration()
+        
+        // 인라인 미디어 재생 허용
+        configuration.allowsInlineMediaPlayback = true
+        
+        // JavaScript로 새 창 열기 허용
+        configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
+        
+        // 공유 프로세스 풀 사용으로 메모리 효율성 향상
+        configuration.processPool = WebProcessPool.shared.processPool
+    }
+}
